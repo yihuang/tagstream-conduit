7bfcad8e3ebd7da9e4bc73417737577d63521667
Dan Burton <danburton.email@gmail.com>
2019-09-06 13:24:20 -0400

    Revert "Demonstration of how to Backpack'ify this package."
    This reverts commit de314fd0fc32110c0ca2a64bfeb2a5def5806c5d.

--- types/Parser/ByteString.hs
+++
@@ -1,7 +0,0 @@
-module Parser.ByteString (module Parser.ByteString, module Data.Attoparsec.ByteString.Char8) where
-
-import Data.ByteString
-import Data.Attoparsec.ByteString.Char8
-
-takeRest :: Parser ByteString
-takeRest = takeByteString
--- types/Parser/Text.hs
+++
@@ -1,7 +0,0 @@
-module Parser.Text (module Parser.Text, module Data.Attoparsec.Text) where
-
-import Data.Text
-import Data.Attoparsec.Text
-
-takeRest :: Parser Text
-takeRest = takeText
